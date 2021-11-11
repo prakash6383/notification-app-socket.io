5dbd178c086e8ede2e81fe97360f5c96f1540433
prakash <prakashvijay4964@gmail.com>
2021-11-10 21:59:40 -0800

    first commit

---
+++ client/src/data.js
@@ -0,0 +1,16 @@
+export const posts = [
+    {
+      id: 1,
+      username:"john",
+      fullname: "John Keller",
+      userImg: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      postImg: "https://images.pexels.com/photos/4534200/pexels-photo-4534200.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+    {
+      id: 2,
+      username:"jack",
+      fullname: "Jack Keller",
+      userImg: "https://images.pexels.com/photos/1172207/pexels-photo-1172207.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+      postImg: "https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+  ];
